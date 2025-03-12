517010c1e0bf1ba1d33ac451bdfc6ab1daa57703
unknown <yaelmedina2031@gmail.com>
2025-03-12 17:07:28 +0800

    made the program check for the bigger number

--- Batch 1/Program1.py
+++ Batch 1/Program1.py
@@ -3,6 +3,9 @@
 user_inp1 = float(input("Please enter a number"))
 user_inp2 = float(input("Enter another number"))
 
-
+if user_inp1 > user_inp2:
+    print(f"{user_inp1} is the bigger number")
+else:
+    print(f"{user_inp2} is the bigger number")
 
 
