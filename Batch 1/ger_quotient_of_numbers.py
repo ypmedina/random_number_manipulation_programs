e93eec7ffd93aea58786d7a8ab1e0484cfe3fbb0
unknown <yaelmedina2031@gmail.com>
2025-03-12 17:39:14 +0800

    Renamed and handled the output for this program

--- Batch 1/ger_quotient_of_numbers.py
+++ Batch 1/ger_quotient_of_numbers.py
@@ -2,3 +2,6 @@
 #Get user input, print the quotient of the two numbers
 user_inp1 = float(input("Please enter a number"))
 user_inp2 = float(input("Enter another number"))
+
+user_quotient = user_inp1 / user_inp2
+print(f"{user_quotient} is the quotient of the first number divided by the second number")
