405c6dfd28a84493f475a567dc55c4157a81a7f2
unknown <yaelmedina2031@gmail.com>
2025-03-12 17:30:49 +0800

    Renamed and handled the output of this porgram

--- Batch 1/get_product_of_numbers.py
+++ Batch 1/get_product_of_numbers.py
@@ -2,3 +2,6 @@
 #Get user input, print the product of two numbers
 user_inp1 = float(input("Please enter a number"))
 user_inp2 = float(input("Enter another number"))
+
+user_prod = user_inp1 * user_inp2
+print(f"{user_prod} is the product of the two entered numbers")
