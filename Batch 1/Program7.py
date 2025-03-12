bc2e6368f3b2e8f6f892045d275bf8478a68bd6a
unknown <yaelmedina2031@gmail.com>
2025-03-12 17:49:38 +0800

    Fixed the input handling

--- Batch 1/Program7.py
+++ Batch 1/Program7.py
@@ -1,4 +1,6 @@
 #Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
 #Create a for function to get 10 user inputs and print the sum of all inputted numbers
+user_sum = 0
 for i in range(10):
-    print(f"Numbers entered{i+1}")
+    user_inp = input(f"Numbers entered{i+1}")
+    user_sum += user_inp
