132c4559195c3eccd07bfc4542e8c660e0ea0837
unknown <yaelmedina2031@gmail.com>
2025-03-12 17:57:23 +0800

    Handled user input

--- Batch 1/Program8.py
+++ Batch 1/Program8.py
@@ -1,2 +1,5 @@
 # Prog08: Create a program that ask user to input 10 numbers. Print how many are odd numbers.
 #Create a for function to get 10 user inputs and print how many odd numbers there are in the inputted numbers
+for i in range(10):
+    user_inp = input(f"Numbers entered {i+1}")
+    
