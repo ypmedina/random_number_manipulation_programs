2764086e262c27866400ba0f99823c8ec2a5b1c4
unknown <yaelmedina2031@gmail.com>
2025-03-12 17:48:10 +0800

    Thinking of how to do get the sum of the numbers entered

--- Batch 1/Program7.py
+++ Batch 1/Program7.py
@@ -1,2 +1,4 @@
 #Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
 #Create a for function to get 10 user inputs and print the sum of all inputted numbers
+for i in range(10):
+    print(f"Numbers entered{i+1}")
