c08578377123672e63aa388664e9b0868b5fdb4d
unknown <yaelmedina2031@gmail.com>
2025-03-12 17:00:33 +0800

    made user input possible for program 1 in batch 1

--- Batch 1/Program1.py
+++ Batch 1/Program1.py
@@ -1,5 +1,8 @@
 #Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
 #Get user input, print bigger number
+user_inp1 = float(input("Please enter a number"))
+user_inp2 = float(input("Enter another number"))
+
 
 
 
