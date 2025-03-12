4942262b06566571fd5f4aee7428943a07cfb9fc
unknown <yaelmedina2031@gmail.com>
2025-03-12 17:43:27 +0800

    Renamed and handled the output for this program

--- Batch 1/get_first_number_raised_second_number.py
+++ Batch 1/get_first_number_raised_second_number.py
@@ -2,3 +2,6 @@
 #Get user input, print the first number inputted raised to the 2nd number
 user_inp1 = float(input("Please enter a number"))
 user_inp2 = float(input("Enter another number"))
+
+user_exp = user_inp1 ** user_inp2
+print(f"{user_exp} is the first number raised to the second number")
