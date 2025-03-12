711937c359df3633810e4d3b417dd57d82a3b91b
unknown <yaelmedina2031@gmail.com>
2025-03-12 18:04:53 +0800

    Finished this program

---
+++ Batch 1/zero_to_one_hundred_even_numbers.py
@@ -0,0 +1,7 @@
+#Prog09: Create a program that print all the even numbers starting from 0 to 100. (Use for loop)
+#Create a for function which has a start, stop, step pf (0, 101, 2) and print it
+for i in range(0, 101, 2):
+    if i > 100:
+        break
+    else:
+        print(i)
