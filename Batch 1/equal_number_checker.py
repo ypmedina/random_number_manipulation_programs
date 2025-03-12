b638b37a68f0034c2f302772d2a1556bd389e3ae
unknown <yaelmedina2031@gmail.com>
2025-03-12 17:20:10 +0800

    Renamed program 2 and made it able to detect the equal numbers

--- Batch 1/equal_number_checker.py
+++ Batch 1/equal_number_checker.py
@@ -3,3 +3,7 @@
 user_inp1 = float(input("Please enter a number"))
 user_inp2 = float(input("Enter another number"))
 
+if user_inp1 == user_inp2:
+    print("The numbers entered are equal")
+else:
+    print("The entered numbers are not equal")
