ea85d922daa941d4f8a7ad00b79f70a71a096a05
unknown <yaelmedina2031@gmail.com>
2025-03-14 00:44:59 +0800

    Made the program able to identify the lower number and update the current lower number

--- Batch 3/Program4.py
+++ Batch 3/Program4.py
@@ -1,12 +1,15 @@
 #Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
 #Use the while loop again, check for the lowest inputted number and make that value the set number so that
 #It will not read a higher number and call it the lowest
-
+lowest = 0
 while True:
     try:
         user_inp = int(input("Please enter a number "))
-
-        
+        if user_inp < lowest:
+            lowest = user_inp
+            print(f"The new lowest number is {lowest}")
+        else:
+            print(f"The entered number is higher than {lowest}")
     except ValueError:
         break
 
