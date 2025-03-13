5c74ecaa00eeaf5419da698ba991e1a91304b338
unknown <yaelmedina2031@gmail.com>
2025-03-13 09:54:04 +0800

    Finished the code for two programs

--- Batch 2/Program2.py
+++ Batch 2/Program2.py
@@ -1,1 +1,8 @@
 #Prog02: Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
+user_inp1 = float(input("Please enter a number: "))
+user_inp2 = float(input("Enter another number: "))
+
+if user_inp1 == user_inp2:
+    print("The numbers entered are equal")
+else:
+    print("The numbers entered are not equal")
