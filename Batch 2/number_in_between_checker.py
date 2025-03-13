390c66973ef8486f88919a21f1fe365c1a71a4bf
unknown <yaelmedina2031@gmail.com>
2025-03-13 10:40:01 +0800

    Fixed and renamed the program

--- Batch 2/number_in_between_checker.py
+++ Batch 2/number_in_between_checker.py
@@ -2,5 +2,8 @@
 user_inp1 = int(input("Please enter a number: "))
 user_inp2 = int(input("Enter another number: "))
 
+if user_inp1 > user_inp2:
+    user_inp1, user_inp2 = user_inp2, user_inp1
+
 for i in range (user_inp1+1, user_inp2, 1):
     print(i)
