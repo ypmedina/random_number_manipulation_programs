57157623745b4ea09ad18610c624d84d6498259f
unknown <yaelmedina2031@gmail.com>
2025-03-13 22:04:47 +0800

    Rearranged the code to get the desired output. Still working on it

--- Batch 3/non_duplicate_number_displayer.py
+++ Batch 3/non_duplicate_number_displayer.py
@@ -1,14 +1,12 @@
 #Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
 #use a for function to get 10 inputs, figure out how to check for number duplicates
 numbers = []
-duplicates = []
+non_dupe= []
 for i in range(10):
     user_inp = int(input(f"Numbers entered: {i+1} "))
+    numbers.append(user_inp)
 
-    if user_inp in numbers:
-        duplicates.append(user_inp)
-    else:
-        numbers.append(user_inp)
+if numbers.count():
+    non_dupe.append(numbers)
 
-print(numbers)
-print(duplicates)
+print(non_dupe)
