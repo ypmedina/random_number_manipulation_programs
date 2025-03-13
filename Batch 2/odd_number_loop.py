0559169dbffba1407e675305f9a49352deaa9f36
unknown <yaelmedina2031@gmail.com>
2025-03-13 10:30:05 +0800

    Fixed and finished the code

--- Batch 2/odd_number_loop.py
+++ Batch 2/odd_number_loop.py
@@ -1,5 +1,8 @@
 #Prog08: Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
-odd_num = 1
+odd_num = -1
 while True:
     odd_num += 2
-    print(odd_num)
+    if odd_num >= 100:
+        break
+    else:
+        print(odd_num)
