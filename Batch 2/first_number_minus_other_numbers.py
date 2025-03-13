7093a9ebd6f15f238bbe4c09d19313bcdef85ad7
unknown <yaelmedina2031@gmail.com>
2025-03-13 10:11:22 +0800

    Fixed the code

--- Batch 2/first_number_minus_other_numbers.py
+++ Batch 2/first_number_minus_other_numbers.py
@@ -1,6 +1,7 @@
 #Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
-total = 0
+total = []
 for i in range(10):
-    user_inp = int(input(f"Number {i+1}: "))
-    total -= user_inp
-print(f"{total} is the first number minus the remaining numbers")
+    user_inp = float(input(f"Number {i+1}: "))
+    total.append(user_inp)
+    difference = total[0] - sum(total[1:])
+print(f"{difference} is the first number minus the remaining numbers")
