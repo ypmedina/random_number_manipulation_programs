f68436887ff7ab080839b2cfbe89c6591c0f9028
unknown <yaelmedina2031@gmail.com>
2025-03-14 01:41:29 +0800

    Made a working program and renamed the file to be more descriptive

--- Batch 3/input_sorting_loop.py
+++ Batch 3/input_sorting_loop.py
@@ -4,11 +4,15 @@
 #make a list to store all numbers, use a while loop with try and except functions, put the user input
 #in the list and use sort function
 
+#well that was easier than expected
 numbers =[]
-
 while True:
     try:
         user_inp = int(input("Enter a number: "))
+        numbers.append(user_inp)
+        numbers.sort()
+        print(numbers)
 
     except ValueError:
+        print("\nThe input is invalid, exiting program now")
         break
