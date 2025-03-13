35c1e7d2defe601fc21d9717e64eb8bc8798aaab
unknown <yaelmedina2031@gmail.com>
2025-03-14 01:36:51 +0800

    Made the pseudocode and initialized a list and made a basic shile loop

--- Batch 3/Program5.py
+++ Batch 3/Program5.py
@@ -1,1 +1,14 @@
-#Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from lowest to highest. Clue: sort() function
+#Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid.
+#Display the number from lowest to highest. Clue: sort() function
+
+#make a list to store all numbers, use a while loop with try and except functions, put the user input
+#in the list and use sort function
+
+numbers =[]
+
+while True:
+    try:
+        user_inp = int(input("Enter a number: "))
+
+    except ValueError:
+        break
