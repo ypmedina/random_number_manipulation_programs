5579f000df86fb9bffca095e4ee6870430c6dda0
unknown <yaelmedina2031@gmail.com>
2025-03-13 21:38:46 +0800

    Made the program able to handle inputs

--- Batch 3/Program1.py
+++ Batch 3/Program1.py
@@ -1,1 +1,5 @@
 #Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
+#use a for function to get 10 inputs, figure out how to check for number duplicates
+
+for i in range(10):
+    user_inp = int(input(f"Numbers entered: {i+1} "))
