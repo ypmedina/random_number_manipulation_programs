e11b10d6c9fff967253f864fb1ca308aafe9bfbd
unknown <yaelmedina2031@gmail.com>
2025-03-13 10:36:43 +0800

    Made code for the two programs

--- Batch 2/Program10.py
+++ Batch 2/Program10.py
@@ -1,1 +1,6 @@
 #Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
+user_inp1 = int(input("Please enter a number: "))
+user_inp2 = int(input("Enter another number: "))
+
+for i in range (user_inp1+1, user_inp2, 1):
+    print(i)
--- Batch 2/numbers_not_ending_in_zero_or_five_loop.py
+++ Batch 2/numbers_not_ending_in_zero_or_five_loop.py
@@ -1,1 +1,4 @@
 #Prog09: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero or ending five.
+for i in range(101):
+    if i % 10 and i % 5 != 0:
+        print(i)
