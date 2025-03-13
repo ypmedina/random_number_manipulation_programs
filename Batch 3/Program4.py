bc331c9e5692d19ac3b8caebe032a5d1af043744
unknown <yaelmedina2031@gmail.com>
2025-03-14 00:31:56 +0800

    Made a pseudocode and made a simple while loop

--- Batch 3/Program4.py
+++ Batch 3/Program4.py
@@ -1,1 +1,15 @@
 #Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
+#Use the while loop again, check for the lowest inputted number and make that value the set number so that
+#It will not read a higher number and call it the lowest
+
+while True:
+    try:
+        user_inp = int(input("Please enter a number "))
+
+        
+    except ValueError:
+        break
+
+
+
+
