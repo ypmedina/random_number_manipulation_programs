4878078da559bb66b6e63524f919a2a48376d008
unknown <yaelmedina2031@gmail.com>
2025-03-13 23:15:16 +0800

    Made a pseudocode

--- Batch 3/Program3.py
+++ Batch 3/Program3.py
@@ -1,1 +1,4 @@
 #Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display "Unique" after input when the inputted number don't have duplicate. Display "Duplicate" after input when the inputted number have duplicate.
+#Make a "while" statement and use "try" and "except" functions to catch invalid inputs
+#For the unique and dupes, just use the same technique as the previous programs
+
