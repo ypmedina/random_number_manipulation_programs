1fb6f976604bc0e43b615618a8418e9bae6b45b7
unknown <yaelmedina2031@gmail.com>
2025-03-14 17:49:53 +0800

    Made a pseudocode

--- batch_four/prog5.py
+++ batch_four/prog5.py
@@ -1,1 +1,3 @@
 #Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
+#use a while loop, make a list, get the len of the list and the sum of the values in the list and then divide them
+#use a def function to make the average program
