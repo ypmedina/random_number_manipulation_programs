2ca1a951d7eb4c686d8eda828019e856c52e224f
unknown <yaelmedina2031@gmail.com>
2025-03-14 18:06:54 +0800

    Used a basic average calculating program and finished the code

--- batch_four/prog5.py
+++ batch_four/prog5.py
@@ -1,3 +1,14 @@
 #Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
 #use a while loop, make a list, get the len of the list and the sum of the values in the list and then divide them
 #use a def function to make the average program
+numbers = []
+
+while True:
+    try:
+        user_inp = int(input("Enter a number: "))
+        numbers.append(user_inp)
+
+        avg = sum(numbers) / len(numbers)
+    except ValueError:
+        print(avg)
+        break
