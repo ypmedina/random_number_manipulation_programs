06b76790fc209dc9bf436d25d8bf6ec8ed332e06
unknown <yaelmedina2031@gmail.com>
2025-03-14 16:27:37 +0800

    Made a pseudocode and made the program handle user inputs

--- batch_four/prog1.py
+++ batch_four/prog1.py
@@ -1,4 +1,14 @@
 #Prog01: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
+#Use a 'for' function for the user input, store the inputs in a list and display all the duplicate numbers in that list
+#using another 'for' function
+
+numbers = []
+for i in range(10):
+    user_inp = int(input(f"Numbers entered {i+1}: "))
+    numbers.append(user_inp)
+
+
+
 
 
 
