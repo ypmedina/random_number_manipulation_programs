90cd57461576a398e4d3b4e28d9adc51e9ba82c4
unknown <yaelmedina2031@gmail.com>
2025-03-20 21:06:24 +0800

    I put these programs in the wrong repository, oops

--- batch_five/8.py
+++
@@ -1,9 +0,0 @@
-"""Prog08: Create a program that ask the user to input their fullname. Print the number of characters in the input.
-Example:
-Input: Juan Dela Cruz
-Output: 14"""
-
-user_input = input("Please enter your full name: ")
-chara_amount = len(user_input)
-
-print(chara_amount)
